8997068286a59611bc61867a17c4c21807857363
hamidehalaee <hamidehalaeeyasar@gmail.com>
2021-04-01 01:57:08 -0700

    change amqp module

--- src/message/amqp.module.ts
+++ src/message/amqp.module.ts
@@ -1,21 +1,38 @@
-import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+//import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { amqpService } from './amqp.service';
 import { amqpController } from './amqp.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
 @Module({
     imports:[ConfigModule.forRoot(),
-        RabbitMQModule.forRoot(RabbitMQModule, {
-        exchanges: [
-          {
-            name: 'exchange1',
-            type: 'topic',
+      ClientsModule.register([
+        {
+          name: 'RSA_SRVICE',
+          transport: Transport.RMQ,
+          options:{
+            urls: [process.env.URLL],
+            queue: 'RSA_queue',
+            queueOptions:{
+              durable: false
+            },  
           },
-        ],
-        uri: process.env.URI,
-        connectionInitOptions: { wait: false },
-      })],
+        },
+      ]),
+    ],
     providers:[amqpService],
     controllers:[amqpController],
 })
 export class MessageModule{}
+
+// RabbitMQModule.forRoot(RabbitMQModule, {
+//   exchanges: [
+//     {
+//       name: 'exchange1',
+//       type: 'topic',
+//     },
+//   ],
+//   uri: process.env.URI,
+//   connectionInitOptions: { wait: false },
+// })
